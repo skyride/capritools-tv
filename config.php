093b506285203e1784067bb37e8871d04983de1a
Adam Findlay <skylinerspeeder@gmail.com>
2015-05-13 15:53:32 +0100

    cleared config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $title = "Capri TV";							//Site title
-$hls_url = "http://skyride.org:8181/";			//URL to the HLS root
+$hls_url = "http://example.com/hls/";			//URL to the HLS root
 $tv_urn = "/tv/";								//URN of the TV page
 $hls_location = "/home/hls/";					//Document root for the HLS files
 
